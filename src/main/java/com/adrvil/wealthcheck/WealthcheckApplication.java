86ace6f5acc9b9815224fe25672da3a6d2d4722a
Adrian Villamin <adrianvillamin0612@gmail.com>
2025-08-27 16:27:59 +0800

    Initial commit

---
+++ src/main/java/com/adrvil/wealthcheck/WealthcheckApplication.java
@@ -0,0 +1,13 @@
+package com.adrvil.wealthcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthcheckApplication.class, args);
+	}
+
+}
